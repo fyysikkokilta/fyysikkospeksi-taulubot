0b33789d970dd7e238270dbb4191f996b3e27078
Niko Savola <niko.savola@aalto.fi>
2021-10-29 14:20:08 +0300

    Lint by refactoring to f-string literal

--- taulubot/__init__.py
+++ taulubot/__init__.py
@@ -7,6 +7,6 @@ __all__ = []
 for filename in [os.path.splitext(i)[0] for i in os.listdir(os.path.dirname(__file__)) if
                  os.path.splitext(i)[1] in pyfile_extensions]:
     if not filename.startswith('__'):
-        __all__.append(importlib.import_module('.%s' % filename, __package__))
+        __all__.append(importlib.import_module(f'.{filename}', __package__))
 
 del os, importlib, pyfile_extensions
